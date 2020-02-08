d1ab1522636ace520c960c8e3001f3a5f80f0ce6
winterprogram <58723353+winterprogram@users.noreply.github.com>
2020-02-09 01:08:44 +0530

    changes till now with some basic js

---
+++ ex.js
@@ -0,0 +1,9 @@
+// var fName = prompt("what's is your first name?");
+// var lName = prompt("what is your last name?");
+var age = prompt("what's user age?");
+// 
+// console.log("name " + fName + " " + lName);
+
+var yu = age * 365.25;
+console.log("age " + yu);
+alert("age: " + yu);

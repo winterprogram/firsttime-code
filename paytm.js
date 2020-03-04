b2168986c60b607425ed208aa781dae61ce68a05
winterprogram <chakladar.sandeep4@gmail.com>
2020-03-04 21:58:27 +0530

    button toggle is working

--- paytm.js
+++ paytm.js
@@ -1,28 +1,35 @@
-$(document).ready(()=>{
-    $("#blankCheckbox").click(() => {
-        $("#main").text("Changed")
-        $("#blankCheckbox").click(() => {
+$(document).ready(() => {
+    $("#blankCheckbox").on("click", () => {
+        if (($("#main").text()) === "Primary") {
+            $("#main").text("Changed")
+        } else {
             $("#main").text("Primary")
-        })
-    });
-})
+        }
+        //    for hide and show in text field
+        // $( ".gg" ).autocomplete({
+        //     source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]
+        //   });
+        // })
 
-let a = () =>{
+    });
+
+
+let a = () => {
     $.ajax({
-        type:"GET",
-        dataType:"json",
-        URL:"",
-        success:(data)=>{
+        type: "GET",
+        dataType: "json",
+        URL: "",
+        success: (data) => {
 
         },
-        error:(data)=>{
+        error: (data) => {
 
         },
         timeout: 1000,
-        beforeSend:()=>{
+        beforeSend: () => {
 
         },
-        Complete:() =>{
+        Complete: () => {
 
         }
     })
